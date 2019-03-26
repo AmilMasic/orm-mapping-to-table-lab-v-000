a3c69d7d3bdbb9113ceaefbbd1e95ac7ea14f0d7
Amil Masic <amil.masic@gmail.com>
2019-03-26 08:53:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def self.create_table
-    sql = <<- SQL
+    sql =<<- SQL
     CREATE TABLE IF NOT EXIST students (
       id INTEGER PRIMARY KEY,
       name TEXT,
